ccbfd4b1f9992430fa27eb2a46e516411a26dc68
Aaron Goldsmith <agoldsmith@alumni.scu.edu>
2019-06-19 00:15:20 -0700

    update package 1
    format

--- public/app.js
+++ public/app.js
@@ -1,48 +1,55 @@
 getResults();
 // Loads results onto the page
-function getResults() {
+function getResults()
+{
   // Empty any results currently on the page
   $("#results").empty();
   // Grab all of the current articles
-  $.getJSON("/articles", function(data) {
-    for (var i = 0; i < data.length-1; i++) {     
-    $("#results").prepend( 
+  $.getJSON("/articles", function (data)
+  {
+    for (var i = 0; i < data.length - 1; i++) {
+      $("#results").prepend(
 
 
-                   " <div id='wrapper' class=' rounded mx-1 p-1 text-center border bg-secondary rounded-3'>"+
-                   "<button data-id="+data[i]._id+" class='btn float-left m-3 rounded-circle btn-outline-success text-success  save'>&#9829;</button>"+
-                   "<button data-id="+data[i]._id+" class=' delete float-right m-4 px-2 py-0 btn btn-outline-danger'>&times;</button>"+ 
-                   "<div class='bg-white rounded mb-1 w-80'><img class='my-3 logo mx-auto' src='https://www.thoughtco.com/thmb/g6QxWI4GWet9HeUT4NEqTIJ7B_g=/3325x664/filters:fill(auto,1)/The-Onion-581247455f9b58564cb87f03.jpg' width=250 height=50 /></div>"+
-                    "<div class='data-entry card text-left p-2' data-id=" + data[i]._id+">"+
-                      "<h4 class='dataTitle text-left' data-id="+data[i]._id+">"+data[i].headline+"</h4>"+
-                      "<hr class='my-2'>"+
-                     "<div class='card-body my-1'><img id='article'class=' p-3 float-right col-md-5 ' src="+data[i].image+"></img><div>"+
-                     "<p class='text-left mb-2 p-relative' id='summary'>"+ data[i].shortsum +
-                     "<br><a class='full mx-3 pt-1' target='_blank' href=" + data[i].anchor +
-                    "><small>Read Full Story</small></a>"+
-                     "</div></p></div>")
+        " <div id='wrapper' class=' rounded mx-1 p-1 text-center border bg-secondary rounded-3'>" +
+        "<button data-id=" + data[i]._id + " class='btn float-left m-3 rounded-circle btn-outline-success text-success  save'>&#9829;</button>" +
+        "<button data-id=" + data[i]._id + " class=' delete float-right m-4 px-2 py-0 btn btn-outline-danger'>&times;</button>" +
+        "<div class='bg-white rounded mb-1 w-80'><img class='my-3 logo mx-auto' src='https://www.thoughtco.com/thmb/g6QxWI4GWet9HeUT4NEqTIJ7B_g=/3325x664/filters:fill(auto,1)/The-Onion-581247455f9b58564cb87f03.jpg' width=250 height=50 /></div>" +
+        "<div class='data-entry card text-left p-2' data-id=" + data[i]._id + ">" +
+        "<h4 class='dataTitle text-left' data-id=" + data[i]._id + ">" + data[i].headline + "</h4>" +
+        "<hr class='my-2'>" +
+        "<div class='card-body my-1'><img id='article'class=' p-3 float-right col-md-5 ' src=" + data[i].image + "></img><div>" +
+        "<p class='text-left mb-2 p-relative' id='summary'>" + data[i].shortsum +
+        "<br><a class='full mx-3 pt-1' target='_blank' href=" + data[i].anchor +
+        "><small>Read Full Story</small></a>" +
+        "</div></p></div>")
     }
   });
 }
-$(document).on("click","#scraper",function(){
-  try{$.ajax({
-    type: "GET",
-    dataType: "json",
-    url: "/scrape",
-  }).then(function(){
-    getResults()
-    location.reload();
-  })}
-  catch{ console.log('ran into problem')}
+$(document).on("click", "#scraper", function ()
+{
+  try {
+    $.ajax({
+      type: "GET",
+      dataType: "json",
+      url: "/scrape",
+    }).then(function ()
+    {
+      getResults()
+      location.reload();
+    })
+  }
+  catch{ console.log('ran into problem') }
 });
 // When user clicks the delete button for a note
-$(document).on("click", ".delete", function(event) {
+$(document).on("click", ".delete", function (event)
+{
   // Save the p tag that encloses the button
   var parent = $(this).parent();
-  if(parent.hasClass('activated')){
-    parent.toggle('activated',false);
+  if (parent.hasClass('activated')) {
+    parent.toggle('activated', false);
   }
-  else{
+  else {
     parent.toggle('activated')
   }
   console.log($(this).attr('data-id'))
@@ -52,14 +59,16 @@ $(document).on("click", ".delete", function(event) {
   $.ajax({
     type: "delete",
     url: "/delete/" + selected,
-    success: function(response) {
+    success: function (response)
+    {
       $(this).parent().remove();
     }
   });
 });
 
 // When user click's on note title, show the note, and allow for updates
-$(document).on("click", ".dataTitle", function() {
- 
+$(document).on("click", ".dataTitle", function ()
+{
+
 
 });
--- server.js
+++ server.js
@@ -9,12 +9,12 @@ require('./models/Article')
 var PORT = process.env.PORT || 3000;
 
 
- // Initialize Express
- // Setup [ using body-parser, dev logging, static folder]
+// Initialize Express
+// Setup [ using body-parser, dev logging, static folder]
 
 var app = express();
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true})
+app.use(bodyParser.urlencoded({ extended: true })
 );
 
 
@@ -22,75 +22,90 @@ var db = require('./models')
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraper";
 mongoose.connect(MONGODB_URI);
 
-  
+
 
 // Retrieve data from the db
-app.get("/articles", function(req, res) {
+app.get("/articles", function (req, res)
+{
   // Find all results from the Onion collection in the db
   db.Article.find({})
-    .then(function(dbArticles) {
+    .then(function (dbArticles)
+    {
       res.json(dbArticles);
     })
-    .catch(function(err) {
+    .catch(function (err)
+    {
       res.json(err);
     });
 });
-app.get("/articles/:id", function(req, res) {
+app.get("/articles/:id", function (req, res)
+{
   db.Article.findOne({ _id: req.params.id })
-  .then(function(dbArticle) {
-    res.json(dbArticle);
-  })
-  .catch(function(err) {
-    res.json(err);
-  });
+    .then(function (dbArticle)
+    {
+      res.json(dbArticle);
+    })
+    .catch(function (err)
+    {
+      res.json(err);
+    });
   // .populate("note")
-    
+
 });
 
-app.delete("/delete/:id",function(req,res){
+app.delete("/delete/:id", function (req, res)
+{
   console.log("attempting to delete")
-  db.Article.deleteOne({_id:req.params.id},function(){res.send(true)})  
+  db.Article.deleteOne({ _id: req.params.id }, function () { res.send(true) })
 })
 
 
 // Scrape data from one site and place it into the mongodb db
-app.get("/clearall",function(req,res){
+app.get("/clearall", function (req, res)
+{
   db.Article.deleteMany({})
   res.send(true);
 })
 
-app.get("/scrape", function(req, res) {
-  axios.get("https://www.theonion.com/").then(function(response) {
-     var $ = cheerio.load(response.data);
-     $(".post-wrapper").each(function(i, element){ // if this doesn't work try with `this`
+app.get("/scrape", function (req, res)
+{
+  axios.get("https://www.theonion.com/").then(function (response)
+  {
+    var $ = cheerio.load(response.data);
+    $(".post-wrapper").each(function (i, element)
+    { // if this doesn't work try with `this`
       const headline = $(this).find(".headline").text();                   // don't touch
       const anchor = $(this).find("article").find("figure a").attr("href") // don't touch
       const unique = $(this).find("article").attr('id').split("_")[1] // don't touch
       const shortsum = $(this).children().find(".entry-summary").text() // don't touch
       const image = $(this).children().find("source").attr('data-srcset') || 'https://picsum.photos/200/300' // don't touch
       var postObj = {
-            headline : headline,
-            anchor : anchor,
-            unique: unique,
-            shortsum : shortsum,
-            image : image
+        headline: headline,
+        anchor: anchor,
+        unique: unique,
+        shortsum: shortsum,
+        image: image
       }
 
-      if(headline && image){
-        db.Article.create(postObj).then(function(dbArticle){
+      if (headline && image) {
+        db.Article.create(postObj).then(function (dbArticle)
+        {
           console.log(dbArticle);
           res.json(dbArticle);
-        }).catch(function(err){
+        }).catch(function (err)
+        {
           throw err;
         })
       }
     });
     // res.send("finished scraping")
-  }).catch(function(err){
+  }).catch(function (err)
+  {
     throw err;
   });
 });
 // Listen on port 3000
-app.listen(PORT, function() {
+app.listen(PORT, function ()
+{
   console.log(`App running on port ${PORT}!`);
 });
